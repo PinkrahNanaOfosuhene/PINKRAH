78f79d3fcea2936289a40b3695cb68790f17145c
Pinkrah Nana Ofosuhene <127850769+PinkrahNanaOfosuhene@users.noreply.github.com>
2025-02-05 15:05:37 +0000

    Add files via upload

---
+++ formula.f90
@@ -0,0 +1,20 @@
+! E=mc2
+
+PROGRAM energy
+
+IMPLICIT NONE
+
+! Declaration of variables
+REAL :: m, E
+INTEGER ::c
+
+! ASsignining variables
+m= 2.2
+c = 30000000
+
+! Formula
+E=m*c**2
+
+PRINT*,'The answer E is ',E
+
+END PROGRAM energy
